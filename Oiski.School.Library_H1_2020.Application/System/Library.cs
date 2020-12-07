915377e1aa32c24824056ef4e883f36033877098
Mike Mortensen <mike2137@elevcampus.dk>
2020-12-07 12:10:38 +0100

    Closes #11
    + Extended Library class to include functionality for manipulating and storing books.

--- Oiski.School.Library_H1_2020.Application/System/Library.cs
+++ Oiski.School.Library_H1_2020.Application/System/Library.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Oiski.School.Library_H1_2020.System.Items;
 using Oiski.School.Library_H1_2020.Users;
 
 namespace Oiski.School.Library_H1_2020.System
@@ -25,12 +27,13 @@ namespace Oiski.School.Library_H1_2020.System
         }
         public string Name { get; }
         private readonly List<Loanee> loanees = new List<Loanee>();
+        private readonly List<Book> books = new List<Book>();
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns>A formated string containing the name of the library and the current date</returns>
-        public string GetData ()
+        public string GetData()
         {
             return $"{Name} - {DateTime.Now.ToShortDateString()}";
         }
@@ -41,7 +44,7 @@ namespace Oiski.School.Library_H1_2020.System
         /// <param name="_name"></param>
         /// <param name="_email"></param>
         /// <returns><see langword="true"/> if a <see cref="Loanee"/> with the same <paramref name="_email"/> does <strong>not</strong> already exist; Otherwise <see langword="false"/></returns>
-        public bool CreateLoanee (string _name, string _email)
+        public bool CreateLoanee(string _name, string _email)
         {
             if ( loanees.Find(item => item.Email.ToLower() == _email.ToLower()) != null )
             {
@@ -57,7 +60,7 @@ namespace Oiski.School.Library_H1_2020.System
         /// </summary>
         /// <param name="_id"></param>
         /// <returns><see langword="true"/> if the <see cref="Loanee"/> could be removed; Otherwise <see langword="false"/></returns>
-        public bool RemoveLoanee (int _id)
+        public bool RemoveLoanee(int _id)
         {
             Loanee loanee = loanees.Find(item => item.ID == _id);
             if ( loanee != null )
@@ -73,7 +76,7 @@ namespace Oiski.School.Library_H1_2020.System
         /// </summary>
         /// <param name="_id"></param>
         /// <returns><see langword="true"/> if the <see cref="Loanee"/> could be removed; Otherwise <see langword="false"/></returns>
-        public bool RemoveLoanee (string _email)
+        public bool RemoveLoanee(string _email)
         {
             Loanee loanee = loanees.Find(item => item.Email.ToLower() == _email.ToLower());
             if ( loanee != null )
@@ -90,7 +93,7 @@ namespace Oiski.School.Library_H1_2020.System
         /// </summary>
         /// <param name="_id"></param>
         /// <returns>The first occurrence that matches the <paramref name="_id"/></returns>
-        public Loanee GetLoanee (int _id)
+        public Loanee GetLoanee(int _id)
         {
             return loanees.Find(item => item.ID == _id);
         }
@@ -99,16 +102,133 @@ namespace Oiski.School.Library_H1_2020.System
         /// </summary>
         /// <param name="_email"></param>
         /// <returns>The first occurrence that matches the <paramref name="_email"/></returns>
-        public Loanee GetLoanee (string _email)
+        public Loanee GetLoanee(string _email)
         {
             return loanees.Find(item => item.Email.ToLower() == _email.ToLower());
+        }
+
+        public bool CreateBook(string _title, string _author, string _isbnCode, out Book _book)
+        {
+            if ( GetBook(_isbnCode) != null )
+            {
+                books.Add(new Book(_title, _author, _isbnCode));
+                return true;
+            }
+
+            return false;
+        }
+        public bool CreateBook(string _title, string _author, string _category, string _isbnCode, out Book _book)
+        {
+            if ( CreateBook(_title, _author, _isbnCode, out _book) )
+            {
+                _book.Category = _category;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveBook(string _isbnCode)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_isbnCode"></param>
+        /// <returns><see langword="true"/> if the book could be borrowed; Otherwise <see langword="false"/></returns>
+        public bool BorrowBook(string _isbnCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_isbnCode"></param>
+        /// <returns><see langword="true"/> if the book could be reserved; Otherwise <see langword="false"/></returns>
+        public bool ReserveBook(string _isbnCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_isbnCode"></param>
+        /// <returns><see langword="true"/> if the book could be returned; Otherwise <see langword="false"/></returns>
+        public bool ReturnBook(string _isbnCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Searches the collection for a specific <see cref="CreateBook"/> with the matching <paramref name="_isbnCode"/>
+        /// </summary>
+        /// <param name="_isbnCode"></param>
+        /// <returns>The first occurrence of a <see cref="CreateBook"/> that matches <paramref name="_isbnCode"/></returns>
+        public Book GetBook(string _isbnCode)
+        {
+            return books.Find(item => item.ISBNCode == _isbnCode);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_title"></param>
+        /// <returns>A <see cref="IReadOnlyList{T}"/> that contains all <see cref="CreateBook"/>s where the <see cref="Book.Title"/> contains <paramref name="_title"/></returns>
+        public IReadOnlyList<Book> GetBooksByTitle(string _title)
+        {
+            return books.Where(item => item.Title.Contains(_title)) as IReadOnlyList<Book>;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_category"></param>
+        /// <returns>A <see cref="IReadOnlyList{T}"/> that contains all <see cref="CreateBook"/>s where the <see cref="Book.Category"/> contains <paramref name="_category"/></returns>
+        public IReadOnlyList<Book> GetBooksByCategory(string _category)
+        {
+            return books.Where(item => item.Category.Contains(_category)) as IReadOnlyList<Book>;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_author"></param>
+        /// <returns>A <see cref="IReadOnlyList{T}"/> that contains all <see cref="CreateBook"/>s where the <see cref="Book.Author"/> contains <paramref name="_author"/></returns>
+        public IReadOnlyList<Book> GetBooksByAuthor(string _author)
+        {
+            return books.Where(item => item.Author.Contains(_author)) as IReadOnlyList<Book>;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_dateOfLending"></param>
+        /// <returns>A <see cref="IReadOnlyList{T}"/> that contains all <see cref="CreateBook"/>s where the <see cref="Book.DateOfLending"/> matches <paramref name="_dateOfLending"/></returns>
+        public IReadOnlyList<Book> GetBooksByDate(DateTime _dateOfLending)
+        {
+            return books.Where(item => item.DateOfLending == _dateOfLending) as IReadOnlyList<Book>;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_isBorrowed"></param>
+        /// <returns>A <see cref="IReadOnlyList{T}"/> that contains all <see cref="CreateBook"/>s where the <see cref="Book.IsBorrowed"/> matches <paramref name="_isBorrowed"/></returns>
+        public IReadOnlyList<Book> GetBooksByStatus(bool _isBorrowed)
+        {
+            return books.Where(item => item.IsBorrowed == _isBorrowed) as IReadOnlyList<Book>;
         }
 
         /// <summary>
         /// Create a new instance of type <see cref="Library"/> where <paramref name="_name"/> is set
         /// </summary>
         /// <param name="_name"></param>
-        private Library (string _name)
+        private Library(string _name)
         {
             Name = _name;
         }
